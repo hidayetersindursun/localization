34020b40694ecab596afa3e505af95e3e1886b8c
hidayetersindursun <h.e.dursun@gmail.com>
2022-02-03 18:18:19 +0300

    update weights function added.

--- assignment4/filter2d.py
+++ assignment4/filter2d.py
@@ -4,6 +4,7 @@ from torch import angle
 from sim.plot2d import plot
 import random as r
 import math
+import numpy as np
 
 
 class Position:
@@ -89,12 +90,41 @@ class Particle(Robot):
 
     def probability_density_function(self, mu, sigma, x):
         ### START STUDENT CODE
-        return 0
+        pdf = (np.exp((((x - mu)/sigma)**2)*(-0.5)))/(sigma*np.sqrt(2*np.pi))
+        return pdf
         ### END STUDENT CODE
 
     def update_weight(self, robot_measurements):
         ### START STUDENT CODE
-        self.weight = 0
+        for p in self.measurements: # particle's measurements
+            best_match = 0
+            for r in robot_measurements: # robot's measurements
+                distance_match = self.probability_density_function(r.distance, self.distance_sigma, p.distance)
+                # normalize match to 1 so we can treat distance and angle as equal weight
+                distance_match = distance_match/self.distance_distribution_peak
+                distance_match *= self.distance_weight
+                
+                # Need to use minimum angle
+                # 10 vs 350 degrees = 20 degrees difference
+                diff_angle = abs(p.angle-r.angle)
+                if diff_angle > math.pi:
+                    diff_angle = abs(diff_angle - math.pi*2)
+                angle_match = self.probability_density_function(0, self.angle_sigma, diff_angle)
+                angle_match = angle_match / self.angle_distribution_peak
+                angle_match *= self.angle_weight
+                # match ile aslında distance ve angle weightlerinden hangisini ne ölçüde kullancağımıza bakıyoruz.
+                # match = distance_match + angle_match --> bu şekilde de yapabilirdik. biri kötü biri iyi olursa bu şekilde dengeler
+                match = distance_match * angle_match #iki değer de iyi olmak zorunda, biri kötü olursa patlar.
+                if match > best_match:
+                    best_match = match
+            self.weight += best_match
+        # Normalize weights based on the number of poles within range.
+        if len(robot_measurements) ==0:
+            return 
+        self.weight /= len(robot_measurements)
+        #Square weight to give more resamples to the best particles.
+        self.weight *= self.weight
+        
         ### END STUDENT CODE
 
 
